3b34744ca235da371fbbaacbc8f0735f0f972655
Hasan Ocak <ocakhasan99@gmail.com>
2020-11-21 19:14:09 +0300

    new data

--- fetch.py
+++ fetch.py
@@ -13,26 +13,29 @@ def change_num(data):
 
 def get_latest_data():
 
-    if datetime.now().hour >= 20:
-        response = requests.get("https://covid19.saglik.gov.tr/covid19api?getir=sondurum")
-        print("\nconnected to website\n")
+    response = requests.get("https://covid19.saglik.gov.tr/covid19api?getir=sondurum")
+    print("\nconnected to website\n")
 
-        last_day = list(response.json())[0]
-        confirmed = change_num(last_day['toplam_vaka'])
-        recovered = change_num(last_day['toplam_iyilesen'])
+    last_day = list(response.json())[0]
+    confirmed = change_num(last_day['toplam_vaka'])
+    recovered = change_num(last_day['toplam_iyilesen'])
 
-        deaths = change_num(last_day['toplam_vefat'])
-        tests = change_num(last_day["gunluk_test"])
-        date = change_date(last_day["tarih"])
+    deaths = change_num(last_day['toplam_vefat'])
+    tests = change_num(last_day["gunluk_test"])
+    date = change_date(last_day["tarih"])
 
-        new_row = {"Country/Region": "Turkey", "Latitude": 38.9637, "Longitude": 35.2433, "Confirmed":confirmed, "Recovered": recovered, "Deaths": deaths, "Date": date, "Tests": tests}
+    new_row = {"Country/Region": "Turkey", "Latitude": 38.9637, "Longitude": 35.2433, "Confirmed":confirmed, "Recovered": recovered, "Deaths": deaths, "Date": date, "Tests": tests}
 
-        df = pd.read_csv("turkey_covid19_all.csv")
-        if date != df["Date"].values[-1]:
-            df = df.append(new_row, ignore_index=True)
-            df.to_csv("turkey_covid19_all.csv", index=False)
+    df = pd.read_csv("turkey_covid19_all.csv")
+    if date != df["Date"].values[-1]:
+        df = df.append(new_row, ignore_index=True)
+        df.to_csv("turkey_covid19_all.csv", index=False)
 
-            print("Created new data for {}\n".format(date))
-            print("New dataset is created")
-        else:
-            print("Dataset is up to date")
+        print("Created new data for {}\n".format(date))
+        print("New dataset is created")
+    else:
+        print("Dataset is up to date")
+
+
+if __name__ == "__main__":
+    get_latest_data()
