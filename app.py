a99628be6cde02a6fe7de694bf0c966b4ee7180d
Hasan Ocak <ocakhasan99@gmail.com>
2021-04-02 21:55:15 +0300

    added folium support

--- app.py
+++ app.py
@@ -4,10 +4,15 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 from fetch import *
+from streamlit_folium import folium_static
+import folium
+import json
 
 st.title('Corona Virus Statistics in Turkey')
 
 DATA_URL = "https://raw.githubusercontent.com/ocakhasan/covid_data/master/turkey_covid.csv" 
+CITIES_URL = "https://raw.githubusercontent.com/ocakhasan/covid_data/master/city_risks.csv"
+GEOSON_DATA = "cities.json"
 
 def barchart(values, labels):
     fig = go.Figure(data=[go.Pie(labels=labels,
@@ -22,13 +27,15 @@ def barchart(values, labels):
 def load_data():
     data = pd.read_csv(DATA_URL)
     data['Date'] = pd.to_datetime(data['Date'])
-    return data
+    cities = pd.read_csv(CITIES_URL)
+    geo_str = json.dumps(json.load(open(GEOSON_DATA, 'r', encoding="utf-8"))) # harita verisi
+    return data, cities, geo_str
 
 # Load 10,000 rows of data into the dataframe.
-df = load_data()
+df, cities, geo_str = load_data()
 
 
-df['Active'] = df['Confirmed'] - df['Recovered'] - df['Deaths'] 
+df['Active'] = df['Confirmed'] - df['Recovered'] - df['Death'] 
 st.subheader('Raw data')
 st.write(df)
 # Notify the reader that the data was successfully loaded.
@@ -38,7 +45,7 @@ st.write(df)
 
 st.subheader('Total Corona Virus Numbers in Turkey')
 
-values=[df['Active'].iloc[-1], df['Recovered'].iloc[-1], df['Deaths'].iloc[-1]]
+values=[df['Active'].iloc[-1], df['Recovered'].iloc[-1], df['Death'].iloc[-1]]
 colors = ['gold', 'mediumturquoise', 'darkorange']
 labels=['Active','Recovered','Deaths']
 fig = barchart(values, labels)
@@ -47,19 +54,58 @@ st.plotly_chart(fig)
 
 #Daily Cases
 labels=['Confirmed','Recovered','Deaths']
-st.subheader('Daily '+ str(df['Date'].iloc[-1]) +' Statistics')
+st.subheader('Daily '+ str(df['Date'].iloc[-1].strftime("%Y/%m/%d")) +' Statistics')
 
-values=[df['Confirmed'].iloc[-1] -df['Confirmed'].iloc[-2], df['Recovered'].iloc[-1] - df['Recovered'].iloc[-2], df['Deaths'].iloc[-1] -df['Deaths'].iloc[-2]]
+values=[df['Daily_Confirmed'].iloc[-1], df['Daily_Recovered'].iloc[-1], df['Daily_Death'].iloc[-1]]
 
 fig = barchart(values, labels)
 
 st.plotly_chart(fig)
 
+st.subheader("Risk Cases in the Map with colors")
+
+m = folium.Map(location=[39.9208, 35], zoom_start=5.4,tiles="cartodbpositron")
+
+scale = np.linspace(cities["risks"].min() - 1, cities["risks"].max() + 1, 6, dtype=int).tolist()
+latitudes = cities['latitudes'].values
+longitudes = cities['longitudes'].values
+locations = list(zip(latitudes, longitudes))
+cpth = folium.Choropleth(
+            geo_data=geo_str, # harita verisi
+            data=cities, # gostermek istedigimiz veri
+            columns=['cities', 'risks'], # istenilen sutunlar
+            fill_color='BuPu',
+            key_on='feature.properties.name',
+            legend_name="Number of cases per 100000 people").add_to(m)
+    
+cpth.geojson.add_child(
+    folium.features.GeoJsonTooltip(['name'], labels=False)
+)
+folium.LayerControl().add_to(m)
+folium_static(m)
+
+st.subheader("Risk Cases in the Map with numbers")
+
+m = folium.Map(location=[39.9208, 35], zoom_start=5.4,tiles="cartodbpositron")
+for i in range(cities.shape[0]):
+    folium.Circle(
+        radius=cities.loc[i, 'risks'] * 100,
+        location=[cities.loc[i, 'latitudes'], cities.loc[i, 'longitudes']],
+        popup=cities.loc[i, 'cities']+ "-" + str(cities.loc[i, "risks"]), 
+        color="#3186cc",
+        fill=True,
+        fill_color="#3186cc"
+).add_to(m)
+    
+folium_static(m)
+
+
+
 #Total Confirmed Cases
 st.subheader("Confirmed Cases Over Time")
 st.write("Let's see the how confirmed cases changed over time")
 
-fig = px.bar(df,x='Date',y='Confirmed',color_discrete_sequence=['#5DADE2'])
+fig = px.bar(df,y='Confirmed',color_discrete_sequence=['#5DADE2'])
 fig.update_layout(template='plotly_dark')
 
 
@@ -67,65 +113,56 @@ st.plotly_chart(fig)
 
 #Daily Cases
 st.subheader('Case Number in Each Day')
-df['New_Cases'] = df['Confirmed'] - df['Confirmed'].shift(1)
-fig = px.bar(df,x='Date',y='New_Cases',color_discrete_sequence=['#1E8449'])
+fig = px.bar(df,y='Daily_Confirmed',color_discrete_sequence=['#1E8449'])
 fig.update_layout(template='plotly_dark')
 
 st.plotly_chart(fig)
 
 #Total Death Cases
 st.subheader('Total Death Cases')
-fig = px.bar(df,x='Date',y='Deaths',color_discrete_sequence=['#7D3C98'])
+fig = px.bar(df,y='Death',color_discrete_sequence=['#7D3C98'])
 fig.update_layout(template='plotly_dark')
 st.plotly_chart(fig)
 
 #Daily Death Cases
 st.subheader('Death Numbers in Each Day')
-df['New_Deaths'] = df['Deaths'] - df['Deaths'].shift(1)
-fig = px.bar(df,x='Date',y='New_Deaths',color_discrete_sequence=['#E67E22'])
+fig = px.bar(df,y='Daily_Death',color_discrete_sequence=['#E67E22'])
 fig.update_layout(template='plotly_dark')
 st.plotly_chart(fig)
 
 #Test Numbers
 st.subheader('Test Numbers in Each Day')
-fig = px.bar(df,x='Date',y='Tests',color_discrete_sequence=['#5D6D7E'])
+fig = px.bar(df,y='Test',color_discrete_sequence=['#5D6D7E'])
 fig.update_layout(template='plotly_dark')
 st.plotly_chart(fig)
 
 #Total Recovered Cases
 st.subheader('Total Recovered Cases')
-fig = px.bar(df,x='Date',y='Recovered',color_discrete_sequence=['#5199FF'])
+fig = px.bar(df,y='Recovered',color_discrete_sequence=['#5199FF'])
 fig.update_layout(template='plotly_dark')
 st.plotly_chart(fig)
 
 #Daily Recovered Cases
 st.subheader('Recovered Numbers in Each Day')
-df['Recovered_Day'] = df['Recovered'] - df['Recovered'].shift(1)
-fig = px.bar(df,x='Date',y='Recovered_Day',color_discrete_sequence=['#C0392B'])
+fig = px.bar(df,y='Daily_Recovered',color_discrete_sequence=['#C0392B'])
 fig.update_layout(template='ggplot2')
 st.plotly_chart(fig)
 
-#Tested Positive versus Negative
-st.subheader('Tested People vs Positive Cases')
-fig = go.Figure(data=[
-go.Bar(name='Tested', y=df['Date'], x=df['Tests'], orientation='h'),
-go.Bar(name='Positive', y=df['Date'], x=df['New_Cases'], orientation='h')])
-fig.update_layout(barmode='stack',width=900, height=600)
-st.plotly_chart(fig)
+
 
 #Corona Virus Statistics in Turkey
 st.subheader('Corona Virus Statistics in Turkey')
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=df['Date'], y=df['Confirmed'],
+fig.add_trace(go.Scatter( y=df['Confirmed'],
                     mode='lines+markers',marker_color='blue',name='Total Cases'))
 
-fig.add_trace(go.Scatter(x=df['Date'], y=df['Recovered'],
+fig.add_trace(go.Scatter(y=df['Recovered'],
                     mode='lines+markers',marker_color='green',name='Recovered Cases'))
 
-fig.add_trace(go.Scatter(x=df['Date'], y=df['Deaths'],
+fig.add_trace(go.Scatter( y=df['Death'],
                     mode='lines+markers',marker_color='red',name='Deaths Cases'))
 
-fig.add_trace(go.Scatter(x=df['Date'], y=df['Active'],
+fig.add_trace(go.Scatter( y=df['Active'],
                     mode='lines+markers',marker_color='#17202A',name='Active Cases'))
 
 fig.update_layout(template='ggplot2')
